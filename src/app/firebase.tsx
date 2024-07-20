95143654ae8c085e5a64a19992575f25792f7c09
Racheal Abosede <ajiboyeabosederacheal@gmail.com>
2024-07-20 09:29:09 +0200

    Updated backend

--- src/app/firebase.tsx
+++ src/app/firebase.tsx
@@ -8,19 +8,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    // apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    // authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    // projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    // storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    // messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    // appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-
-    apiKey: "AIzaSyCHjz0Y4yNg3fp5oQanJXv79aPKyUYczc8",
-  authDomain: "carefinder-pro.firebaseapp.com",
-  projectId: "carefinder-pro",
-  storageBucket: "carefinder-pro.appspot.com",
-  messagingSenderId: "1088610583368",
-  appId: "1:1088610583368:web:22513163aa5d5ebf13697d"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 
   };
 
